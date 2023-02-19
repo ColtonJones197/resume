9c7a3c3b3cdbc4b32806468a099b63295fcf3edc
Colton Jones <ckjones97@ksu.edu>
2023-02-19 02:31:43 -0600

    began project

---
+++ resume-site/src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<h1 class="text-3xl font-bold text-center">Colton Jones</h1>
+<p>Software | Smash | Fitness | Rackets</p>
+<div class="text-center"><button class="text-center">About Me</button></div>
+
+<style>
+	:global(html) {
+		background-color: #2b2736;
+		color: #f1f1f1;
+		display: flex;
+		justify-content: center;
+		margin-top: 45%;
+	}
+
+	button {
+		border: .2em solid white;
+		margin-top: 1em;
+		transition: .5s;
+		padding: .25em;
+		font-weight: bold;
+		font-size: large;
+	}
+	button:hover
+	{
+		color: #2b2736;
+		background-color: #f1f1f1;
+		border-color: #2b2736;
+	}
+</style>
---
+++ resume-site/src/routes/about/+page.svelte
@@ -0,0 +1,15 @@
+
+<h1>About Me</h1>
+<section>
+	<p></p>
+</section>
+
+<style>
+	:global(html) {
+		background-color: #2b2736;
+		color: #f1f1f1;
+		display: flex;
+		justify-content: center;
+		margin-top: 45%;
+	}
+</style>
